d9a49669252878ba4522efa43336af603ad344da
Simplify Me. <dialyourfoolishfriend@gmail.com>
2025-09-22 22:50:35 +0000

    Initial public release for portfolio review

---
+++ src/components/icons/XIcon.tsx
@@ -0,0 +1,16 @@
+import { SVGProps } from "react";
+
+export function XIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1200 1227"
+      fill="currentColor"
+      {...props}
+    >
+      <path
+        d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L112.383 44.0126H302.483L604.496 507.634L652.001 575.493L1093.52 1181.99H903.416L569.165 687.854V687.828Z"
+      />
+    </svg>
+  );
+}
